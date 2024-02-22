0458bef7d647bde345fcea222a3313e8828402b0
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-02-22 12:25:51 +0100

    theatre-square problem

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 | __Problem code__ | __Problem name__ | __Solution__ |
 | :---: | :---: | :---: |
+| [1A](https://codeforces.com/problemset/problem/1/A) | Theatre Square | [<img src="res/bright-light-bulb.svg">](src/theatre-square/main.cpp) |
 | [4A](https://codeforces.com/problemset/problem/4/A) | Watermelon | [<img src="res/bright-light-bulb.svg">](src/watermelon/main.cpp) |
 | [71A](https://codeforces.com/problemset/problem/71/A) | Way Too Long Words | [<img src="res/bright-light-bulb.svg">](src/way-too-long-words/main.cpp) |
 | [231A](https://codeforces.com/problemset/problem/231/A) | Team | [<img src="res/bright-light-bulb.svg">](src/team/main.cpp) |
---
+++ src/theatre-square/main.cpp
@@ -0,0 +1,35 @@
+// /* 
+//    Author: @imAlessas
+//    Problem: https://codeforces.com/problemset/problem/1/A
+// */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long long n, m, a;
+    cin >> n >> m >> a;
+
+    long long hz_tiles = (n + a - 1) /a;
+    long long ver_tiles = (m + a - 1) /a;
+    
+    cout << hz_tiles * ver_tiles << endl;
+
+
+    return 0;
+} // main
+
+/*
+    Also tried to run the code:
+
+        double n, m, a;
+        cin >> n >> m >> a;
+
+        cout << ceil(n / a) * ceil(m / a) << endl;
+
+    but there was a "Wrong answer on test 9", probably because the inputs were double.
+
+    I also tried using long long inputs with static_cast<double> and the ceil function but the error on the test still occurred.
+*/
