88800c37e06b674b87cbbecec05eb4957861f002
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-02-22 01:29:12 +0100

    added comments

--- src/watermelon/main.cpp
+++ src/watermelon/main.cpp
@@ -1,22 +1,20 @@
-/*          Author: @imAlessas          */
-
-// Guidelines: https://codeforces.com/problemset/problem/4/A
-
+/* 
+   Author: @imAlessas
+   Problem: https://codeforces.com/problemset/problem/4/A
+*/
 
 #include <iostream>
-
-
 using namespace std;
 
 int main() {
     int w;
     cin >> w;
 
-    if ( w % 2 || w <= 2)
+    // Checking if weight is even and greater than 2
+    if (w % 2 || w <= 2)
         cout << "NO" << endl;
     else
         cout << "YES" << endl;
 
-
     return 0;
-}
+} // main
