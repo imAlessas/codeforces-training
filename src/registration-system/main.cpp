a8d7f77674d5b72fce5314dbba6c674d632ba943
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-02-23 18:56:09 +0100

    registration-system problem

--- src/registration-system/main.cpp
+++ src/registration-system/main.cpp
@@ -16,21 +16,19 @@ int main() {
 
     vector<pair<string, int>> names = {};
     for (int i = 0; i < n; i++) {
-        string s = "3";
-        // cin >> s;
+        string s;
+        cin >> s;
         
         auto it = find_if(names.begin(), names.end(), [&s](const pair<string, int>& element) {
             return element.first == s;
         });
 
-        cout << i << endl;
-
-        if(it != names.end()){
+        if(it == names.end()){
             cout << "OK" << endl;
             names.push_back({s, 0});
         } else {
-            cout << s + to_string(it->second) << endl;
             it->second++;
+            cout << s + to_string(it->second) << endl;
         }
     }
 
