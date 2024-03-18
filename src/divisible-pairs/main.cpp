8afec7c32f31a6d039adfcb1afcb005373f46264
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-03-18 11:37:33 +0100

    minor changes

--- src/divisible-pairs/main.cpp
+++ src/divisible-pairs/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 // function definition
 bool is_divisible(const int&, const int&);
-bool is_beautyful_pair(const int&, const int&, const int&, const int&);
+bool is_beautiful_pair(const int&, const int&, const int&, const int&);
 
 int main() {
     int t, n, x, y, a, count;
@@ -29,7 +29,7 @@ int main() {
             
             // time complexity is too high
             for(int i = 0; i < v.size() - 1; i++)
-                if(is_beautyful_pair(v[i], v[v.size() - 1], x, y))
+                if(is_beautiful_pair(v[i], v[v.size() - 1], x, y))
                     count++;
             
         }
@@ -42,9 +42,9 @@ int main() {
 
 
 // check if the pair is a beutiful one
-bool is_beautyful_pair(const int& i, const int& j, const int& x, const int& y){
+bool is_beautiful_pair(const int& i, const int& j, const int& x, const int& y){
     return is_divisible(i + j, x) && is_divisible(j - i, y);
-} // is_beautyful_pair
+} // is_beautiful_pair
 
 
 
