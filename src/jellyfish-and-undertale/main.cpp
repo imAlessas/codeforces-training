f49847b3d5292f76d6f0eab029b8653aec1f40e7
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-16 20:07:38 +0200

    jellyfish-and-undertale problem

--- src/jellyfish-and-undertale/main.cpp
+++ src/jellyfish-and-undertale/main.cpp
@@ -16,39 +16,23 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); 
 
-    int t, a, b, n, count, x;
+    int t, a, b, n;
+    long long x, count;
 
     cin >> t;
 
     while(t--){
 
-        count = 0;
-        
         cin >> a >> b >> n;
-
-        while(b--){
-
-            while(b + x < a && n > 0){
-                b += x;
-                cin >> x;
-                n--;
-            }
-
-            if(b == 1 && n > 0){
-                b = a;
-                cin >> x;
-                n--;
-            }
-            
-            if(b == 1 && n == 0){
-                b = a;
-                n--;
-            }
-
-            count++;
-
-        }
         
+        count = b;
+
+        while(n--){
+
+            cin >> x;
+
+            count = count + (x < a ? x : a - 1);
+        }
 
         cout << count << endl;
         
