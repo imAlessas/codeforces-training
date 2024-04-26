21faf5e4bce27a822605094c8a2694eb5349b62f
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-26 15:28:21 +0200

    minor changes

--- src/product-of-binary-decimals/main.cpp
+++ src/product-of-binary-decimals/main.cpp
@@ -10,28 +10,20 @@ using namespace std;
 
 // function definition
 
+vector<int> get_all_binary_decimals(const int&);
+
+
+bool check_product(const int&, const vector<int>&, int);
+
+
 
 int main() {
 
-    // calculates all the binary decimals from 1 to 5 * 10^5
-    int current;
-    bool flag;
-    vector<int> bin_dec = {};
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
 
-    for(int i = 1; i <= 5 * (10 ^ 5); i++){
-        current = i;
-
-        while(current){
-            if( current % 10 > 1){
-                flag = false;
-                break;
-            }
-            current = current / 10;
-        }
-
-        if(flag)
-            bin_dec.push_back(i);
-    }
+    // pre-processing
+    vector<int> bin_dec = get_all_binary_decimals(5 * 100000);
 
 
     int t, n;
@@ -42,8 +34,64 @@ int main() {
 
         cin >> n;
 
+        if( check_product(n, bin_dec, 0 ) )
+            cout << "YES" << endl;
+        else    
+            cout << "NO" << endl;
 
     }
 
     return 0;
 } // main
+
+
+
+
+
+// checks if the number can be written as a prduct of binary decimals
+bool check_product(const int& n, const vector<int>& bin_dec, int idx){
+
+    if(n == 1)
+        return true;
+
+    for(int i = idx; i < bin_dec.size(); i++){
+
+        if( !(n % bin_dec[i]) )
+            if( check_product(n / bin_dec[i], bin_dec, i) )
+                return true;
+        
+
+    }
+
+    return false;
+} // check_product
+
+
+
+
+
+// calculates all the binary decimals from 1 to upper_limit
+vector<int> get_all_binary_decimals(const int& upper_limit){
+
+    vector<int> binary_decimals = {};
+    int current;
+    bool flag;
+
+    for(int i = upper_limit; i > 0 ; i--){
+        current = i;
+        flag = true;
+
+        while(current){
+            if( current % 10 > 1){
+                flag = false;
+                break;
+            }
+            current = current / 10;
+        }
+
+        if(flag)
+            binary_decimals.push_back(i);
+    }
+
+    return binary_decimals;
+} // get_all_binary_decimals
