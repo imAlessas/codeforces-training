742c19a97dec70fbcddbaa1a35967a4e25e03e34
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-04-27 14:32:30 +0200

    continued anna-and-the-valentines-day-gift

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ This repository contains my solutions to some CodeForces exercises. In the table
 | [`1931B`](https://codeforces.com/problemset/problem/1931/B) | Make Equal | [<img src="res/solution.svg">](src/make-equal/main.cpp) | 800 | 358 ms | 1600 KB |
 | [`1931C`](https://codeforces.com/problemset/problem/1931/C) | Make Equal Again | [<img src="res/solution.svg">](src/make-equal-again/main.cpp) | 1000 | 264 ms | 2100 KB |
 | [`1931D`](https://codeforces.com/problemset/problem/1931/D) | Divisible Pairs | [<img src="res/solution.svg">](src/divisible-pairs/main.cpp) | 1300 | 311 ms | 12900 KB |
-| [`1931E`](https://codeforces.com/problemset/problem/1931/E) | Anna and the Valentine's Day Gift | [<img src="res/thinking.svg">](src/anna-and-the-valentines-day-gift/main.cpp) | 1400 | / ms | / KB |
+| [`1931E`](https://codeforces.com/problemset/problem/1931/E) | Anna and the Valentine's Day Gift | [<img src="res/time-fail.svg">](src/anna-and-the-valentines-day-gift/main.cpp) | 1400 | / ms | / KB |
 | [`1932A`](https://codeforces.com/problemset/problem/1932/A) | Thorns and Coins | [<img src="res/solution.svg">](src/thorns-and-coins/main.cpp) | 800 | 46 ms | 0 KB |
 | [`1941A`](https://codeforces.com/problemset/problem/1941/A) | Rudolf and the Ticket | [<img src="res/solution.svg">](src/rudolf-and-the-ticket/main.cpp) | 800 | 46 ms | 0 KB |
 | [`1944A`](https://codeforces.com/problemset/problem/1944/A) | Destroying Bridges | [<img src="res/solution.svg">](src/destroying-bridges/main.cpp) | 800 | 46 ms | 0 KB |
--- src/anna-and-the-valentines-day-gift/main.cpp
+++ src/anna-and-the-valentines-day-gift/main.cpp
@@ -8,17 +8,50 @@
 
 using namespace std;
 
+
 // function definition
-pair<int, int> find_two_max_index(const vector<int>&);
+
+int count_zeros(const string&);
+
+int max_index(const vector<string>&);
+
+int get_max_zeros_index(const vector<string>&);
+
+string merge_numbers(const string&, const string&);
+
+void reverse_number(string&);
+
+void print_vector(const vector<string>& v){
+    
+    cout << "vector = [ ";
+
+    for(string a : v)
+        cout << a << " ";
+
+    cout << "]" << endl;
+}
+
+
+
+
 
 int main() {
-    int t, n, m, a;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+
+
+    int t, n, m;
+    string a, tmp;
+
+    vector<string> v = {};
+    int zeros_idx, max_idx;
+
     cin >> t;
 
-    vector<int> v = {};
-    pair<int, int> max_idx = {};
-
     while (t--){
+        v.clear();
+
         cin >> n >> m;
 
         while(n--){
@@ -27,22 +60,30 @@ int main() {
         }
 
         while(v.size() > 1){
+
             // Anna's turn
-            // TODO
-
-
+            zeros_idx = get_max_zeros_index(v);
+            reverse_number(v[zeros_idx]);
 
             // Sasha's turn
-            // find the two maximum numbers and merge them by comparing the first digit
-            max_idx = find_two_max_index(v);
-            v[max_idx.first] = merge_numbers(v[max_idx.first], v[max_idx.second]);
-            v.erase(v.begin() + max_idx.second);
+            zeros_idx = get_max_zeros_index(v);
+            tmp = v[zeros_idx];
+            v.erase(v.begin() + zeros_idx);
+
+            max_idx = max_index(v);
+            v[max_idx] = merge_numbers(tmp, v[max_idx]);
+
         }
 
-        if(log10(v[0]) >= m)
-            cout << "Sasha" << endl;
+        // last Anna's turn
+        zeros_idx = get_max_zeros_index(v);
+        reverse_number(v[zeros_idx]);
+
+
+        if(v[0].size() > m)
+            cout << "Sasha"<< endl;
         else
-            cout << "Anna" << endl;
+            cout << "Anna"<< endl;
     }
     
 
@@ -50,30 +91,75 @@ int main() {
 } // main
 
 
-// returns indexes of the two greatest numbers in the array
-pair<int, int> find_two_max_index(const vector<int>& v){
-    int n = v.size();
-    if(n == 1)
-        return {v[0], 0};
 
-    pair<int, int> max_index = {0, 0};
-    for(int i = 0; i < n; i++){
-        if(v[i] >= v[max_index.first]){
-            max_index.second = max_index.first;
-            max_index.first = i;
-        }
-    }
+
+
+// gets the index of the number with the max trailing zeros
+int get_max_zeros_index(const vector<string>& v){
+
+    int max_zero_index = 0;
+
+    for(int i = 1; i < v.size(); i++)
+        if( count_zeros( v[i] ) > count_zeros( v[max_zero_index] ) )
+            max_zero_index = i;
+    
+
+    return max_zero_index;
+
+} // get_max_zeros_index
+
+
+
+// counts the number of trailing zeros of a number n 
+int count_zeros(const string& s){
+    
+    int count = 0;
+
+    for(int i = s.size() - 1; i > 0; i--)
+        if( s[i] == '0' )
+            count++;
+        else
+            break;
+
+    return count;
+} // count_zeros
+
+
+
+// given a number n: a0, a1, ..., aN; this function return the reverse of that number: aN, a(N - 1), ..., a0
+void reverse_number(string& s){
+    
+    reverse(s.begin(), s.end());
+
+    auto it = s.begin();
+    while (it != s.end() && *it == '0') 
+        it++;
+    
+     s.erase(s.begin(), it);
+
+} // reverse_number
+
+
+
+// returns indexes of the number with max zeros at the end and the max number
+int max_index(const vector<string>& v){
+
+    int max_index = 0;
+
+    for(int i = 1; i < v.size(); i++)
+        if( v[i].size() > v[max_index].size() )
+            max_index = i;
+    
+
     return max_index;
+
 } // find_two_max_index
 
 
-// merges two numbers into the biggets they can create
-int merge_numbers(const int& a, const int& b){
-    string a_string = a + "";
-    string b_string = b + "";
 
-    if(stoi(a_string[0] + "") > stoi(b_string[0] + ""))
-        return stoi(a_string + b_string);
+// merges two numbers
+string merge_numbers(const string& zeros, const string& max){
 
-    return stoi(b_string + a_string);
+    return zeros + max;
+
 } // merge_numbers
