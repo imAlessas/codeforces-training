5b6aea60fb724198271d62cc6722ab839c35cf7e
imAlessas <73311498+imAlessas@users.noreply.github.com>
2024-11-11 15:51:26 +0100

    Update shifts-and-sorting
    Still got TLE

--- src/shifts-and-sorting/main.py
+++ src/shifts-and-sorting/main.py
@@ -7,22 +7,9 @@
 # Function definition
 
 
-
 # Take the last characted and shift it at idx=0
 def cycle_string(substr):
     return substr[-1] + substr[:-1]
-
-
-# Checks if the string is sorted
-def is_sorted(s):
-    for i in range(1, len(s)):
-        if s[i - 1] > s[i]:
-            return False
-        
-    return True
-
-
-
 
 
 
@@ -35,21 +22,22 @@ def main():
         count = 0
         for i in range(len(s) - 1):
             
-            if is_sorted(s):
-                break
-
             if s[i] == '1':
 
                 j = i
 
                 # Get to the first 0
-                while j < len(s) and s[j] != '0':
+                while j < len(s) - 1 and s[j] != '0':
                     j += 1
-                j += 1
 
-                count += j - i
+
+                if j == len(s)-1 and s[j] == '1':
+                    break
+
+                count += j - i + 1
 
                 # Cycle the string
+                j += 1
                 s = s[:i] + cycle_string(s[i : j]) + s[j:]
 
         print(count)
